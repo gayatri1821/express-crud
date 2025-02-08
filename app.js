70fdc78ddea8b4736737f928ebbe97144e8106d2
gayatri1821 <gayatrikumbhar47@gmail.com>
2025-02-08 14:24:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+//function display()
+//{
+  //return display
+//}
+ //module.exports=display
+
+ //function display(a,b)
+ //{
+//    return a+b
+ //}
+ //module.exports=display
+
+ function add(a,b)
+ {
+    return a+b
+ }
+ function sub(a,b)
+ {
+    return a-b
+ }
+ module.exports={add,sub}
